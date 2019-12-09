0fd8a883f48ad8a8d918629377418568cc08cb51
Bradley <b.leadbetter@live.co.uk>
2019-12-09 22:27:42 +0000

    Updated slides

--- components/Chart.js
+++ components/Chart.js
@@ -6,12 +6,12 @@ const data = [{name: 'Never', votes: 43}, {name: 'Some', votes: 30}, {name: 'Oft
 
 const Chart = () => (
   <BarChart width={800} height={450} data={data} margin={{ top: 60, right: 20, bottom: 25, left: 20 }}>
-    <XAxis dataKey="name" stroke="#8884d8" hide={false}  />
+    <XAxis dataKey="name" stroke="#add8e6" hide={false}  />
     <YAxis />
     <Tooltip />
 
     <Text scaleToFit={false} width="0"/>
-      <Bar type="monotone" dataKey="votes" fill="#8884d8" barSize={200} >
+      <Bar type="monotone" dataKey="votes" fill="#add8e6" barSize={200} >
       </Bar>
   </BarChart>
 );
